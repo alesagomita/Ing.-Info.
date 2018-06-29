97015b525253c88504595494d3c89dff6945896c
Csandoval <sandovalch.christhian@gmail.com>
2018-06-28 23:10:56 -0500

    Revert "Implementacion class paciente"
    This reverts commit b2914e22de901071f328230cc78917597b07ac75.

--- src/java/capa/negocio/Paciente.java
+++ src/java/capa/negocio/Paciente.java
@@ -8,7 +8,6 @@ package capa.negocio;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
-import java.util.Date;
 
 /**
  *
@@ -18,72 +17,10 @@ import java.util.Date;
 @SessionScoped
 public class Paciente implements Serializable {
 
-    
-    private String apellidos;
-    private String nombres;
-    private String direccion;
-    private String dni;
-    private String telf_fijo;
-    private Date fecha_nac;
-    
-    public Paciente(String apellidos, String nombres, String direccion, String dni, String telf_fijo, Date fecha_nac) {
-        
-        this.apellidos = apellidos;
-        this.nombres = nombres;
-        this.direccion = direccion;
-        this.dni = dni;
-        this.telf_fijo = telf_fijo;
-        this.fecha_nac = fecha_nac;
+    /**
+     * Creates a new instance of Paciente
+     */
+    public Paciente() {
     }
-
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
-    public String getNombres() {
-        return nombres;
-    }
-
-    public void setNombres(String nombres) {
-        this.nombres = nombres;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getDni() {
-        return dni;
-    }
-
-    public void setDni(String dni) {
-        this.dni = dni;
-    }
-
-    public String getTelf_fijo() {
-        return telf_fijo;
-    }
-
-    public void setTelf_fijo(String telf_fijo) {
-        this.telf_fijo = telf_fijo;
-    }
-
-    public Date getFecha_nac() {
-        return fecha_nac;
-    }
-
-    public void setFecha_nac(Date fecha_nac) {
-        this.fecha_nac = fecha_nac;
-    }
-    
-    
     
 }
