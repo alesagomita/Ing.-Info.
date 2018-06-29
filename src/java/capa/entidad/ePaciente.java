5f6bf5f0d83d48276868a84855be151ee11e3aee
Csandoval <sandovalch.christhian@gmail.com>
2018-06-28 23:10:15 -0500

    entidad Paciente
    implementacion de los getters & setters de la clase

---
+++ src/java/capa/entidad/ePaciente.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capa.entidad;
+
+import java.util.Date;
+
+/**
+ *
+ * @author CHRISTHIAN
+ */
+public class ePaciente {
+    
+    private Integer id_paciente;
+    private String apellidos;
+    private String nombres;
+    private String dni;
+    private String direccion;
+    private String tfijo;
+    private Date fnacimiento;
+
+    public Integer getId_paciente() {
+        return id_paciente;
+    }
+
+    public void setId_paciente(Integer id_paciente) {
+        this.id_paciente = id_paciente;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTfijo() {
+        return tfijo;
+    }
+
+    public void setTfijo(String tfijo) {
+        this.tfijo = tfijo;
+    }
+
+    public Date getFnacimiento() {
+        return fnacimiento;
+    }
+
+    public void setFnacimiento(Date fnacimiento) {
+        this.fnacimiento = fnacimiento;
+    }
+    
+    
+    
+}
