424aee9898b7295c62ae16b814e667bd6bddbd3a
Csandoval <sandovalch.christhian@gmail.com>
2018-06-28 22:33:54 -0500

    Implementacion class medico
    Agregando construct, getter & setters de la clase

--- src/java/capa/negocio/Medico.java
+++ src/java/capa/negocio/Medico.java
@@ -17,10 +17,49 @@ import java.io.Serializable;
 @SessionScoped
 public class Medico implements Serializable {
 
-    /**
-     * Creates a new instance of Medico
-     */
-    public Medico() {
+    private String apellidos;
+    private String nombres;
+    private String dni;
+    private String email;
+
+    public Medico(String apellidos, String nombres, String dni, String email) {
+        this.apellidos = apellidos;
+        this.nombres = nombres;
+        this.dni = dni;
+        this.email = email;
+
     }
-    
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
