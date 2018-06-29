aa8e5cfd67b60a373d723bdb05a8ba9db77da36e
alesagomita <karlaalessandragomerosalinas@gmail.com>
2018-06-29 00:54:12 -0500

    agrega datos conexion y usuario

--- src/java/capa/datos/Conexion.java
+++ src/java/capa/datos/Conexion.java
@@ -5,10 +5,75 @@
  */
 package capa.datos;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 /**
  *
- * @author CHRISTHIAN
+ * @author root
  */
 public class Conexion {
+
+    //objetos para conectar al pool en glashfish
+    private String jndi = "jdbc/taller";
+    private Context ctx;
+    private DataSource ds;
+    //objetos para conectar via jdbc normal
+    private String driver = "org.postgresql.Driver";
+    private String connectString="jdbc:postgresql://localhost:5432/Taller?Schema=public";
+    private String user="Essalud";
+    private String password ="sql";
+    //objetos comunes
+    private Connection Cn;
+    
+    public Conexion(){
+        
+    }
+
+   public void ConectaJNDI(){
+       try{
+           //se carga el contexto inicial para realizar operaciones JNDI
+           ctx=new javax.naming.InitialContext();
+           //se busca el recurso correspondiente al pool de conexiones
+           ds= (DataSource) ctx.lookup(this.jndi);
+           Cn=ds.getConnection();
+       }catch (SQLException | NamingException ex){
+           Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE,null,ex);
+       }
+   }
+   public void DesconectarJNDI(){
+       try{
+           Cn.close();
+           ctx.close();
+       }catch(SQLException | NamingException ex){
+           Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE,null,ex);
+       }
+   }
+   public void ConectaJDBC()  {
+       try{
+           Class.forName(driver);
+           Cn=DriverManager.getConnection(connectString,user,password);
+       }catch (ClassNotFoundException | SQLException ex){
+           Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE,null,ex);
+       }
+   }
+   public void DesconectarJDBC(){
+       try{
+           Cn.close();
+       }catch(SQLException ex){
+           Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE,null,ex);
+       }
+   }
+    public Connection getCn() {
+        return Cn;
+    }
+
+    public void setCn(Connection Cn) {
+        this.Cn = Cn;
+    }
     
 }
--- src/java/capa/datos/dUsuario.java
+++ src/java/capa/datos/dUsuario.java
@@ -4,11 +4,65 @@
  * and open the template in the editor.
  */
 package capa.datos;
-
+import capa.entidad.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
- * @author CHRISTHIAN
+ * @author root
  */
 public class dUsuario {
-    
+    private Conexion miBD;
+    public dUsuario(){
+        
+    }
+    public eUsuario of_BuscarUsuario(eUsuario sUsuario){
+        PreparedStatement pst;
+        ResultSet rs;
+        eUsuario rUsuario = new eUsuario();
+        this.miBD =new Conexion();
+        this.miBD.ConectaJNDI();
+        try{
+            pst= this.miBD.getCn().prepareCall("select * from pa_lusuario(?)");
+            pst.setString(1,sUsuario.getUsuario());
+            pst.executeQuery();
+            rs = pst.getResultSet();
+            while(rs.next()){
+                rUsuario.setId_usuario(rs.getInt("id_usuario"));
+                rUsuario.setUsuario(rs.getString("usuario"));
+                rUsuario.setPassword(rs.getString("password"));
+            }
+        }catch(SQLException ex){
+           Logger.getLogger(dUsuario.class.getName()).log(Level.SEVERE,null,ex);
+       }
+        this.miBD.DesconectarJNDI();
+        return rUsuario;
+    }
+    public List<eUsuario> of_ListarUsuario(){
+        List<eUsuario> rListado = new ArrayList<>();
+        eUsuario miUsuario;
+        Statement st;
+        ResultSet rs;
+        
+        this.miBD = new Conexion();
+        this.miBD.ConectaJNDI();
+        try{
+           st = this.miBD.getCn().createStatement();
+           rs = st.executeQuery("select * from pa_usuario_listar()");
+           while(rs.next()){
+               miUsuario = new eUsuario();
+               miUsuario.setId_usuario(rs.getInt("id_usuario"));
+                miUsuario.setUsuario(rs.getString("usuario"));
+                miUsuario.setPassword(rs.getString("password"));
+                rListado.add(miUsuario);
+           }
+       }catch(SQLException ex){
+           Logger.getLogger(dUsuario.class.getName()).log(Level.SEVERE,null,ex);
+       }
+        this.miBD.DesconectarJNDI();
+        return rListado;
+    }
 }
