fe9401d30ffe8b8adfae895168a605915509c27f
alesagomita <karlaalessandragomerosalinas@gmail.com>
2018-06-28 20:43:36 -0500

    implementacion clase usuario

--- src/java/capa/negocio/Usuario.java
+++ src/java/capa/negocio/Usuario.java
@@ -20,7 +20,35 @@ public class Usuario implements Serializable {
     /**
      * Creates a new instance of Usuario
      */
+    
+    private String User;
+    private String Pass;
+    
+    public String getUser(){
+       return User;
+    }
+    public void setUser(String User){
+        this.User = User;
+    }
+    public String getPass(){
+        return Pass;
+    }
+    public void setPass(String Pass){
+        this.Pass = Pass;
+    }
     public Usuario() {
+       this.User = "";
+        this.Pass = "";
+    }
+    public Object ValidarAcceso(){
+        if (this.User.equals("admin")) {
+            return "acceder";
+            
+        }else{
+            this.setUser("");
+            this.setPass("");
+            return "";
+        }
     }
     
 }
