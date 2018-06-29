91d531a61db31ddf8d1878fa504a09fe04f22868
Csandoval <sandovalch.christhian@gmail.com>
2018-06-28 23:09:53 -0500

    entidad Medico
    Implementacion de getters & setters de la clase

---
+++ src/java/capa/entidad/eMedico.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capa.entidad;
+
+/**
+ *
+ * @author CHRISTHIAN
+ */
+public class eMedico {
+    
+    private Integer id_medico;
+    private String apellidos;
+    private String nombres;
+    private String dni;
+    private String email;
+
+    public Integer getId_medico() {
+        return id_medico;
+    }
+
+    public void setId_medico(Integer id_medico) {
+        this.id_medico = id_medico;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+}
